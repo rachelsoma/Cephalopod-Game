5c22ceed8bf257f0bd43b552b2625a91b2c5831f
Rachel Soma <rachel@rachelsoma.com>
2016-09-06 17:39:29 +1000

    initial commit

---
+++ game.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include<iomanip>
+
+using namespace std;
+//methods
+
+//get board size
---
+++ gameApp.cpp
@@ -0,0 +1,28 @@
+//Main program
+
+#include <iostream>
+#include<iomanip>
+
+#include "cephalopod.h"
+
+using namespace std;
+
+
+
+int main() {
+  Game a;
+
+  int numRows, numCols, bsizeIn,board;
+//choose size of board from a list
+  a.boardSize(numRows,numCols,bsizeIn);
+
+//make board using size selected
+  a.makeBoard(a,numRows,numCols);
+  a.printBoard(numRows,numCols,a);
+//human move
+  a.placeDie(numRows,numCols,a);
+
+  a.printBoard(numRows,numCols,a);
+
+  return 0;
+};
