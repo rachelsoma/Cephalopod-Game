743a73ba72630594963c93b12f8e99e1dca53645
Rachel Soma <rachel@rachelsoma.com>
2016-09-14 12:28:56 +1000

    weds

--- cephalopod.h
+++ cephalopod.h
@@ -15,13 +15,11 @@ using namespace std;
 class Board {
 public:
   Board();
-  void play();
-  void boardSize();
-  void makeBoard();
   void printBoard();
-  void initBoard();
   void placeDie();
   bool isFull();
+  void randomPlayer();
+  void UDLR();
 
 protected:
   /* using _ prefix to easily ideantify private */
@@ -108,7 +106,9 @@ bool Board::isFull() {
   }
   return true;
 };
+void Board::humanMove(){
 
+};
 void Board::placeDie() {
   int col;
   int row;
@@ -127,13 +127,38 @@ void Board::placeDie() {
   row = row - 1;
 
 // check move
-  if (brd[row][col] == 0) { //checks if seat is taken or not **make own function if time**
-    brd[row][col] = 1; //places a 1 on the board
-  } else {
+  while (brd[row][col] != 0) { //while loop to validate input
     cout << "That move is invalid" << endl;
+    do {
+      cout << "Choose a col: ";
+      cin >> col;
+    } while (col < 1 || col > _numCols); //while loop to validate input
+    col = col - 1;
+
+    do {
+      cout << "Choose a row: ";
+      cin >> row;
+    } while (row < 1 || row > _numRows);
+    row = row - 1;
   }
+  brd[row][col] = 1; //places a 1 on the board};
 };
 
+void Board::randomPlayer() {
+  int col;
+  int row;
+  /* initialize random seed: */
+  srand (time(NULL));
 
+  do {
+    row = rand() % _numRows;
+    col = rand() % _numCols;
+  } while (brd[row][col] != 0);  //checks if tile is taken or not **make own function if time**
+  brd[row][col] = -1; //places a 1 on the board
+};
+
+void Board::UDLR(){
+  int udlr[4] = brd[row]-1[col], brd[row]+1[col], brd[row][col]-1, brd[row][col]+1;
+};
 
 #endif /* CEPHALOPOD_H_ */
--- gameApp.cpp
+++ gameApp.cpp
@@ -2,32 +2,30 @@
 
 #include <iostream>
 #include <iomanip>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 
 #include "cephalopod.h"
 //#include "Player.h"
 using namespace std;
 
 int main() {
-  bool playOn = true;
+
   Board a;
-
- // a.boardSize(); //User selects board size
-
-  //a.makeBoard(); //make board using size selected
-
   a.printBoard(); //display current board with moves
-  //human move
 
   //**TO DO** a while loop that checks if playOn status is TRUE if not, game ends
 do {
 
   a.placeDie(); //a human move
+  a.isFull();
+  //a.printBoard();
 
+  a.randomPlayer();
+  a.isFull();
   a.printBoard();
 
-
- ; // tests if board is full
-} while (!a.isFull());
+} while (!a.isFull()); // tests if board is full
 
 cout << "Game over" <<endl;
 
